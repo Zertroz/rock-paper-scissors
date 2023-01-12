8aff3e29e2a0863e1218f1ed04365dab2a9b6716
Kass Daniello <zertroz@gmail.com>
2023-01-12 12:45:49 -0500

    Make variant gamemode

--- game.js
+++ game.js
@@ -6,13 +6,16 @@ class Game {
     this.choices;
   }
 
-  setChoices() {
+  setChoices(gameType) {
+    this.gameType = gameType
     if (this.gameType === 'classic') {
-      this.choices = ['rock', 'paper', 'scissors']
-    }
-  }
+      this.choices = ['rock', 'paper', 'scissors'];
+    } else if (this.gameType === 'variant') {
+      this.choices = ['samurai', 'ronin', 'shinobi', 'oni', 'brush'];
+    };
+  };
 
-  checkWin(selection, computerChoice) {
+  runGame(selection, computerChoice) {
     if (selection === computerChoice) {
       return 'draw';
     } else if (selection === 'rock' && computerChoice === 'scissors') {
@@ -30,7 +33,40 @@ class Game {
     }
   }
 
+  runGameVar(selection, computerChoice) {
+    if (selection === computerChoice) {
+      return 'draw';
+    } else if (selection === 'brush' && (computerChoice === 'shinobi' || computerChoice === 'oni')) {
+      this.player1.wins ++;
+      return 'win';
+    } else if (selection === 'samurai' && (computerChoice === 'oni' || computerChoice === 'brush')) {
+      this.player1.wins ++;
+      return 'win';
+    } else if (selection === 'ronin' && (computerChoice === 'samurai' || computerChoice === 'brush')) {
+      this.player1.wins ++;
+      return 'win';
+    } else if (selection === 'oni' && (computerChoice === 'ronin' || computerChoice === 'shinobi')) {
+      this.player1.wins ++;
+      return 'win';
+    } else if (selection === 'shinobi' && (computerChoice === 'samurai' || computerChoice === 'ronin')) {
+      this.player1.wins ++;
+      return 'win';
+    } else {
+      this.player2.wins ++;
+      return "lose";
+    }
+  }
+
+  checkWin(pSelection, cSelection) {
+    if (this.gameType === 'classic') {
+      this.runGame(pSelection, cSelection)
+    } else if (this.gameType === 'variant') {
+      this.runGameVar(pSelection, cSelection)
+    }
+  }
+
   resetGameType() {
     this.gameType = '';
+    this.choices = '';
   }
 }
